a12558cfe4523b898a6b02b5d6ca0ed31afd0d12
giannis <giannisduke@gmail.com>
2017-08-27 19:19:48 +0300

    remove test folders

--- site/web/app/themes/eboy/template-custom.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Template Name: Custom Template
- */
-?>
-<?php get_template_part('templates/unit', 'hero'); ?>
-<?php get_template_part('templates/unit', 'cv'); ?>
-<?php get_template_part('templates/unit', 'what_i_do'); ?>
