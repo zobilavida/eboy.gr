e5b1374e657ad677414ea4325d365f3a95a8a8e8
eboy <giannisduke@gmail.com>
2017-08-30 21:41:59 +0300

    mail 1

--- site/web/app/themes/eboy_theme/functions.php
+++ site/web/app/themes/eboy_theme/functions.php
@@ -174,3 +174,39 @@ function echo_first_image( $postID ) {
 		}
 	}
 }
+
+
+add_action( 'publish_post', 'itsg_create_sitemap' );
+add_action( 'publish_page', 'itsg_create_sitemap' );
+
+function itsg_create_sitemap() {
+
+    $postsForSitemap = get_posts(array(
+        'numberposts' => -1,
+        'orderby' => 'modified',
+        'post_type'  => array( 'post', 'page' ),
+        'order'    => 'DESC'
+    ));
+
+    $sitemap = '<?xml version="1.0" encoding="UTF-8"?>';
+    $sitemap .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+
+    foreach( $postsForSitemap as $post ) {
+        setup_postdata( $post );
+
+        $postdate = explode( " ", $post->post_modified );
+
+        $sitemap .= '<url>'.
+          '<loc>' . get_permalink( $post->ID ) . '</loc>' .
+          '<lastmod>' . $postdate[0] . '</lastmod>' .
+          '<changefreq>monthly</changefreq>' .
+         '</url>';
+      }
+
+    $sitemap .= '</urlset>';
+
+    $fp = fopen( ABSPATH . 'sitemap.xml', 'w' );
+
+    fwrite( $fp, $sitemap );
+    fclose( $fp );
+}
