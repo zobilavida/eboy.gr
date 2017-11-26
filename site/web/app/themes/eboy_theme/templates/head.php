d264a9fee9f2b5b999c527a49f4a68bc5f3b119c
eboy <giannisduke@gmail.com>
2017-11-26 23:08:35 +0200

    eboy - meta description

--- site/web/app/themes/eboy_theme/templates/head.php
+++ site/web/app/themes/eboy_theme/templates/head.php
@@ -3,5 +3,6 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="google-site-verification" content="OSRva4F52QRkQz5kbgzB3HWe2BXumVaYvsyHmzUdnLQ" />
+  <meta name="description" content="Wordpress developer and UI/UX Graphic designer based in Athens Greece.">
   <?php wp_head(); ?>
 </head>
