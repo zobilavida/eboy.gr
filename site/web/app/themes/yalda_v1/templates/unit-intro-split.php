959b92bb9b2f7506abb875b05ac5276458cc7574
eboy <giannisduke@gmail.com>
2017-12-23 14:02:52 +0200

    yalda - cat fix

--- site/web/app/themes/yalda_v1/templates/unit-intro-split.php
+++ site/web/app/themes/yalda_v1/templates/unit-intro-split.php
@@ -4,7 +4,7 @@
 
 
     <?php    // The Query
-       $recentposts = get_posts('numberposts=1&category=17');
+       $recentposts = get_posts('numberposts=1&category=38');
        foreach ($recentposts as $post) :
            setup_postdata($post);  $featured_img_url = get_the_post_thumbnail_url(get_the_ID(),'full');  ?>
            <div class="col-lg-12 my-auto p-5"><a href="<?php the_permalink() ?>"><h1><?php the_title(); ?></h1></a>
