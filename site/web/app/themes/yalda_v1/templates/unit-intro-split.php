cbe574cca58a768147b48c524c5d6a50a0606597
eboy <giannisduke@gmail.com>
2017-12-23 15:46:18 +0200

    yalda - v2 social

--- site/web/app/themes/yalda_v1/templates/unit-intro-split.php
+++ site/web/app/themes/yalda_v1/templates/unit-intro-split.php
@@ -7,9 +7,25 @@
        $recentposts = get_posts('numberposts=1&category=38');
        foreach ($recentposts as $post) :
            setup_postdata($post);  $featured_img_url = get_the_post_thumbnail_url(get_the_ID(),'full');  ?>
-           <div class="col-lg-12 my-auto p-5"><a href="<?php the_permalink() ?>"><h1><?php the_title(); ?></h1></a>
+           <div class="col-lg-12 my-auto p-5">
+             <a href="<?php the_permalink() ?>"><h1><?php the_title(); ?></h1></a>
              <?php echo apply_filters( 'the_excerpt', $post->post_excerpt ); ?>
+             <div class="social-bar row p-5">
+               <div class="col-6">
+               Facebook
+               </div>
+               <div class="col-6">
+               Twitter
+               </div>
+               <div class="col-6">
+               Instagram
+               </div>
+               <div class="col-6">
+
+               </div>
+             </div>
            </div>
+
            <div class="col-lg-12 right-image" style="background-image: url('<?php echo $featured_img_url ?>'); height: 100%; background-repeat: no-repeat;" >
     tyest
 
