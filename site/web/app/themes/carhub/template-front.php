c4dfe3312a28f497299889558ef564a4b119bfac
eboy <giannisduke@gmail.com>
2018-01-09 11:31:11 +0200

    eboy - header, logo middle

--- site/web/app/themes/carhub/template-front.php
+++ site/web/app/themes/carhub/template-front.php
@@ -6,7 +6,3 @@
 
 <?php get_template_part('templates/unit', 'hero'); ?>
 <?php get_template_part('templates/unit', 'form'); ?>
-<?php get_template_part('templates/unit', 'services'); ?>
-<?php get_template_part('templates/unit', 'map'); ?>
-<?php get_template_part('templates/unit', 'customer'); ?>
-<?php get_template_part('templates/unit', 'contact'); ?>
