6a2e070f0853c9ff5da5036c0990cd8c2dba31b5
eboy <giannisduke@gmail.com>
2018-01-25 23:35:32 +0200

    Yalda - images up

--- site/web/app/themes/yalda_v2/templates/header.php
+++ site/web/app/themes/yalda_v2/templates/header.php
@@ -1,27 +1,10 @@
 <header role="banner">
 <div class="container-fluid ">
-  <div class="row">
-    <div class="col-3 my-auto">
-      <a href="<?= esc_url(home_url('/')); ?>">
-  <img id="logo-main" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/32877/logo-thing.png" width="200" alt="Logo Thing main logo">
-</a>
-      <p>
-      <a class="d-lg-none" data-toggle="collapse" href="#collapseExample" aria-expanded="false" aria-controls="collapseExample" onclick="myFunction(this)">
-    <img src="<?= get_template_directory_uri(); ?>/assets/images/menu.svg" class="menu-icon" alt="Web developer"/>
-</a>
-</p>
 
-
-</div>
-    <div class="col-6">
-
-</div>
-    <div class="col-3">
-
-</div>
-
-</div>
 <div class="row d-flex justify-content-center">
+  <a href="<?= esc_url(home_url('/')); ?>">
+<img id="logo-main" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/32877/logo-thing.png" width="200" alt="Logo Thing main logo">
+</a>
   <div class="collapse" id="collapseExample">
       <?php
           wp_nav_menu( array(
