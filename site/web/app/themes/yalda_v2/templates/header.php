4db8fe632d5345d4f40b78059f458cd5c23c8648
eboy <giannisduke@gmail.com>
2018-01-25 23:49:28 +0200

    Yalta - logo left, images up

--- site/web/app/themes/yalda_v2/templates/header.php
+++ site/web/app/themes/yalda_v2/templates/header.php
@@ -2,9 +2,7 @@
 <div class="container-fluid ">
 
 <div class="row d-flex justify-content-center">
-  <a href="<?= esc_url(home_url('/')); ?>">
-<img id="logo-main" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/32877/logo-thing.png" width="200" alt="Logo Thing main logo">
-</a>
+
   <div class="collapse" id="collapseExample">
       <?php
           wp_nav_menu( array(
