d978b7557dcddedffd44de49a295a7b6c5849c2c
eboy <giannisduke@gmail.com>
2018-03-30 23:19:59 +0300

    Demetrius - functions 1

--- site/web/app/themes/demetrios_v2/functions.php
+++ site/web/app/themes/demetrios_v2/functions.php
@@ -26,3 +26,117 @@ foreach ($sage_includes as $file) {
   require_once $filepath;
 }
 unset($file, $filepath);
+
+
+// Include custom navwalker
+require_once('bs4navwalker.php');
+
+// Register WordPress nav menu
+register_nav_menu('top', 'Top menu');
+
+// Add svg & swf support
+function cc_mime_types( $mimes ){
+    $mimes['svg'] = 'image/svg+xml';
+    $mimes['swf']  = 'application/x-shockwave-flash';
+
+    return $mimes;
+}
+add_filter( 'upload_mimes', 'cc_mime_types' );
+//enable logo uploading via the customize theme page
+
+function themeslug_theme_customizer( $wp_customize ) {
+    $wp_customize->add_section( 'themeslug_logo_section' , array(
+    'title'       => __( 'Logo', 'themeslug' ),
+    'priority'    => 30,
+    'description' => 'Upload a logo to replace the default site name and description     in the header',
+) );
+$wp_customize->add_setting( 'themeslug_logo' );
+$wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize,     'themeslug_logo', array(
+    'label'    => __( 'Logo', 'themeslug' ),
+    'section'  => 'themeslug_logo_section',
+    'settings' => 'themeslug_logo',
+    'extensions' => array( 'jpg', 'jpeg', 'gif', 'png', 'svg' ),
+) ) );
+}
+
+
+add_action ('customize_register', 'themeslug_theme_customizer');
+
+function itsg_create_sitemap() {
+
+    $postsForSitemap = get_posts(array(
+        'numberposts' => -1,
+        'orderby' => 'modified',
+        'post_type'  => array( 'post', 'page', 'product' ),
+        'order'    => 'DESC'
+    ));
+
+    $sitemap = '<?xml version="1.0" encoding="UTF-8"?>';
+    $sitemap .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+
+    foreach( $postsForSitemap as $post ) {
+        setup_postdata( $post );
+
+        $postdate = explode( " ", $post->post_modified );
+
+        $sitemap .= '<url>'.
+          '<loc>' . get_permalink( $post->ID ) . '</loc>' .
+          '<lastmod>' . $postdate[0] . '</lastmod>' .
+          '<changefreq>monthly</changefreq>' .
+         '</url>';
+      }
+
+    $sitemap .= '</urlset>';
+
+    $fp = fopen( ABSPATH . 'sitemap.xml', 'w' );
+
+    fwrite( $fp, $sitemap );
+    fclose( $fp );
+}
+
+add_action( 'publish_post', 'itsg_create_sitemap' );
+add_action( 'publish_page', 'itsg_create_sitemap' );
+add_action( 'save_post_my_post_type', 'itsg_create_sitemap' );
+
+
+/**
+ * Optimize WooCommerce Scripts
+ * Remove WooCommerce Generator tag, styles, and scripts from non WooCommerce pages.
+ */
+add_action( 'wp_enqueue_scripts', 'child_manage_woocommerce_styles', 99 );
+
+function child_manage_woocommerce_styles() {
+	//remove generator meta tag
+	remove_action( 'wp_head', array( $GLOBALS['woocommerce'], 'generator' ) );
+
+	//first check that woo exists to prevent fatal errors
+	if ( function_exists( 'is_woocommerce' ) ) {
+		//dequeue scripts and styles
+		if ( ! is_woocommerce() && ! is_cart() && ! is_checkout() ) {
+			wp_dequeue_style( 'woocommerce_frontend_styles' );
+			wp_dequeue_style( 'woocommerce_fancybox_styles' );
+			wp_dequeue_style( 'woocommerce_chosen_styles' );
+			wp_dequeue_style( 'woocommerce_prettyPhoto_css' );
+      wp_dequeue_style('woocommerce-smallscreen');
+      wp_dequeue_style('woocommerce-layout');
+      wp_dequeue_style('woocommerce-general');
+			wp_dequeue_script( 'wc_price_slider' );
+			wp_dequeue_script( 'wc-single-product' );
+			wp_dequeue_script( 'wc-add-to-cart' );
+			wp_dequeue_script( 'wc-cart-fragments' );
+			wp_dequeue_script( 'wc-checkout' );
+			wp_dequeue_script( 'wc-add-to-cart-variation' );
+			wp_dequeue_script( 'wc-single-product' );
+			wp_dequeue_script( 'wc-cart' );
+			wp_dequeue_script( 'wc-chosen' );
+			wp_dequeue_script( 'woocommerce' );
+			wp_dequeue_script( 'prettyPhoto' );
+			wp_dequeue_script( 'prettyPhoto-init' );
+			wp_dequeue_script( 'jquery-blockui' );
+			wp_dequeue_script( 'jquery-placeholder' );
+			wp_dequeue_script( 'fancybox' );
+			wp_dequeue_script( 'jqueryui' );
+		}
+	}
+
+}
