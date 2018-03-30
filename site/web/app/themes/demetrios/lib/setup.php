b2890fc9aede7072cec66e38147c3b2ba59c9ac1
eboy <giannisduke@gmail.com>
2018-03-30 07:53:52 +0300

    Demetrios is_page();

--- site/web/app/themes/demetrios/lib/setup.php
+++ site/web/app/themes/demetrios/lib/setup.php
@@ -86,7 +86,7 @@ function display_sidebar() {
     is_404(),
     is_front_page(),
     is_page_template('template-custom.php'),
-    is_page_template('template-finder.php'),
+    is_page(),
   ]);
 
   return apply_filters('sage/display_sidebar', $display);
