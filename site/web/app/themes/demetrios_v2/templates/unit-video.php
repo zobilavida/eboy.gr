3b7681e3cca86b80debf4b5841c7fd2d66516629
eboy <giannisduke@gmail.com>
2018-03-31 17:42:25 +0300

    Demetrios 2 - Fix dis path unit-hero

--- site/web/app/themes/demetrios_v2/templates/unit-video.php
+++ site/web/app/themes/demetrios_v2/templates/unit-video.php
@@ -1,3 +1,3 @@
-<section class="home-section bg-dark-30" id="home" data-background="<?= get_template_directory_uri(); ?>/assets/images/section-5.jpg">
+<section class="home-section bg-dark-30" id="home" data-background="<?= get_template_directory_uri(); ?>/dist/images/section-5.jpg">
 <?php do_action( 'demetrios', 'video_background' ); ?>
 </section>
