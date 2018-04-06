d96f2e347feeb79170afffddbf6224321f19e939
eboy <giannisduke@gmail.com>
2018-04-06 17:01:06 +0300

    demetrios 2- video carousel test

--- site/web/app/themes/demetrios_v2/functions.php
+++ site/web/app/themes/demetrios_v2/functions.php
@@ -329,14 +329,93 @@ function video_background(){
 
 ?>
 <section class="home-section bg-dark-30" id="home" data-background="assets/images/finance/finance_header_bg.png">
-  <div class="video-player" data-property="{videoURL:'<?php echo $video_background; ?>', containment:'.home-section', startAt:30, mute:false, autoPlay:true, loop:true, opacity:.5, showControls:false, showYTLogo:false, vol:25}"></div>
+  <!--Carousel Wrapper-->
+  <div id="video-carousel-example2" class="carousel slide carousel-fade" data-ride="carousel">
+      <!--Indicators-->
+      <ol class="carousel-indicators">
+          <li data-target="#video-carousel-example2" data-slide-to="0" class="active"></li>
+          <li data-target="#video-carousel-example2" data-slide-to="1"></li>
+          <li data-target="#video-carousel-example2" data-slide-to="2"></li>
+      </ol>
+      <!--/.Indicators-->
+      <!--Slides-->
+      <div class="carousel-inner" role="listbox">
+          <!-- First slide -->
+          <div class="carousel-item active">
+              <!--Mask color-->
+              <div class="view">
+                  <!--Video source-->
+                  <video class="video-fluid" autoplay loop>
+                      <source src="https://mdbootstrap.com/img/video/Lines.mp4" type="video/mp4" />
+                  </video>
+                  <div class="mask rgba-indigo-light"></div>
+              </div>
 
-  <div class="titan-caption">
-    <div class="caption-content">
-      <div class="font-alt mb-30"><h2><?php echo $video_background_text_1; ?></h2></div>
-      <div class="font-alt mb-40 titan-title-size-4 pb-3"><?php echo $video_background_text_2; ?></div><a class="section-scroll btn btn-border-w btn-round" href="<?php echo $video_background_button_url; ?>"><?php echo $video_background_button; ?></a>
-    </div>
+              <!--Caption-->
+              <div class="carousel-caption">
+                  <div class="animated fadeInDown">
+                      <h3 class="h3-responsive">Light mask</h3>
+                  </div>
+              </div>
+              <!--Caption-->
+          </div>
+          <!-- /.First slide -->
+
+          <!-- Second slide -->
+          <div class="carousel-item">
+              <!--Mask color-->
+              <div class="view">
+                  <!--Video source-->
+                  <video class="video-fluid" autoplay loop>
+                      <source src="https://mdbootstrap.com/img/video/animation-intro.mp4" type="video/mp4" />
+                  </video>
+                  <div class="mask rgba-purple-slight"></div>
+              </div>
+
+              <!--Caption-->
+              <div class="carousel-caption">
+                  <div class="animated fadeInDown">
+                      <h3 class="h3-responsive">Super light mask</h3>
+                  </div>
+              </div>
+              <!--Caption-->
+          </div>
+          <!-- /.Second slide -->
+
+          <!-- Third slide -->
+          <div class="carousel-item">
+              <!--Mask color-->
+              <div class="view">
+                  <!--Video source-->
+                  <video class="video-fluid" autoplay loop>
+                      <source src="https://mdbootstrap.com/img/video/cube.mp4" type="video/mp4" />
+                  </video>
+                  <div class="mask rgba-black-strong"></div>
+              </div>
+
+              <!--Caption-->
+              <div class="carousel-caption">
+                  <div class="animated fadeInDown">
+                      <h3 class="h3-responsive">Strong mask</h3>
+                  </div>
+              </div>
+              <!--Caption-->
+          </div>
+          <!-- /.Third slide -->
+      </div>
+      <!--/.Slides-->
+      <!--Controls-->
+      <a class="carousel-control-prev" href="#video-carousel-example2" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#video-carousel-example2" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+      </a>
+      <!--/.Controls-->
   </div>
+  <!--Carousel Wrapper-->
 </section><?php
 } else { echo "Niente";}
 
