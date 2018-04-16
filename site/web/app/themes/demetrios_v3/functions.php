22fec347dfff7a8ddbcfcc0554834a650d73213b
eboy <giannisduke@gmail.com>
2018-04-17 00:57:22 +0300

    Demetrios 3 - Hide pagination (woocommerce)

--- site/web/app/themes/demetrios_v3/functions.php
+++ site/web/app/themes/demetrios_v3/functions.php
@@ -1175,7 +1175,7 @@ add_filter( 'loop_shop_per_page', 'new_loop_shop_per_page', 20 );
 function new_loop_shop_per_page( $cols ) {
   // $cols contains the current number of products per page based on the value stored on Options -> Reading
   // Return the number of products you wanna show per page.
-  $cols = 4;
+  $cols = 8;
   return $cols;
 }
 
