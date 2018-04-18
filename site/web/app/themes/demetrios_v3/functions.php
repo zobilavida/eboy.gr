cd7646a5897ee91d4ee8e60a0d579e36c5e0665b
eboy <giannisduke@gmail.com>
2018-04-18 08:46:08 +0300

    Demetrios 3 - form

--- site/web/app/themes/demetrios_v3/functions.php
+++ site/web/app/themes/demetrios_v3/functions.php
@@ -1052,7 +1052,7 @@ function store_finder_split(){
     "orderby" => "title",
     "order" => "ASC",
     "posts_per_page" => 35,
-    'eboywp' => true 
+    'eboywp' => true
   );
 
   $query = new WP_Query( $args );
@@ -1114,7 +1114,7 @@ function book(){
             </div>
             </div>
 
-        <div class="container px-4 mt-5 book-appontment">
+        <div class="container px-1 mt-5 book-appontment">
           <div class="row">
             <div class="col-12">
       <?php gravity_form_enqueue_scripts( 1, false ); ?>
@@ -1252,4 +1252,43 @@ foreach ( array( 'pre_term_description' ) as $filter ) {
 }
 foreach ( array( 'term_description' ) as $filter ) {
     remove_filter( $filter, 'wp_kses_data' );
+}
+
+add_filter("gform_field_content", "bootstrap_styles_for_gravityforms_fields", 10, 5);
+function bootstrap_styles_for_gravityforms_fields($content, $field, $value, $lead_id, $form_id){
+
+	// Currently only applies to most common field types, but could be expanded.
+
+	if($field["type"] != 'hidden' && $field["type"] != 'list' && $field["type"] != 'multiselect' && $field["type"] != 'checkbox' && $field["type"] != 'fileupload' && $field["type"] != 'date' && $field["type"] != 'html' && $field["type"] != 'address') {
+		$content = str_replace('class=\'medium', 'class=\'form-control medium', $content);
+	}
+
+	if($field["type"] == 'name' || $field["type"] == 'address') {
+		$content = str_replace('<input ', '<input class=\'form-control\' ', $content);
+	}
+
+  if($field["type"] == 'text') {
+    $content = str_replace('<input ', '<input class=\'form-control\' ', $content);
+  }
+
+	if($field["type"] == 'textarea') {
+		$content = str_replace('class=\'textarea', 'class=\'form-control textarea', $content);
+	}
+
+	if($field["type"] == 'checkbox') {
+		$content = str_replace('li class=\'', 'li class=\'checkbox ', $content);
+		$content = str_replace('<input ', '<input style=\'margin-left:1px;\' ', $content);
+	}
+
+	if($field["type"] == 'radio') {
+		$content = str_replace('li class=\'', 'li class=\'radio ', $content);
+		$content = str_replace('<input ', '<input style=\'margin-left:1px;\' ', $content);
+	}
+
+	return $content;
+
+} // End bootstrap_styles_for_gravityforms_fields()
+add_filter("gform_submit_button", "form_submit_button", 10, 2);
+function form_submit_button($button, $form){
+    return "<button class='button btn btn-default' id='gform_submit_button_{$form["id"]}'><span>Submit</span></button>";
 }
