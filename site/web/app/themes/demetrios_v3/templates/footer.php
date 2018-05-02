a64f67dcecbeaa1c904b77236761da81a4c25a57
eboy <giannisduke@gmail.com>
2018-05-02 05:27:29 +0300

    Demetrios 3 - fix search, remove

--- site/web/app/themes/demetrios_v3/templates/footer.php
+++ site/web/app/themes/demetrios_v3/templates/footer.php
@@ -1,11 +1,5 @@
 <footer class="content-info">
-  <div id="slider-top" class="side-panel side-panel-top">
-    <div class="container-fluid h-100 test">
-        <div class="row h-100 test">
 
-  </div>
-  </div>
-    </div>
   <div class="container">
     <div class="row">
     <?php dynamic_sidebar('sidebar-footer'); ?>
