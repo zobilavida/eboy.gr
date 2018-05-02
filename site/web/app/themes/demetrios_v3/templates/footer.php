072cdbcb89b020cd40a583a9c3e6c95e239a1096
eboy <giannisduke@gmail.com>
2018-05-02 05:23:10 +0300

    Demetrius 3 - fix search, empty

--- site/web/app/themes/demetrios_v3/templates/footer.php
+++ site/web/app/themes/demetrios_v3/templates/footer.php
@@ -2,7 +2,6 @@
   <div id="slider-top" class="side-panel side-panel-top">
     <div class="container-fluid h-100 test">
         <div class="row h-100 test">
-        <?php do_action( 'custom_store_spilt_finder_2', 'store_finder_split_2' ); ?>
 
   </div>
   </div>
