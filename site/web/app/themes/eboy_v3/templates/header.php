84c797c5f8b2a37333cec506b5af506a34392879
eboy <giannisduke@gmail.com>
2018-06-24 19:13:57 +0300

    eboy_v3 - responsivenes

--- site/web/app/themes/eboy_v3/templates/header.php
+++ site/web/app/themes/eboy_v3/templates/header.php
@@ -1,6 +1,6 @@
 
 	<header class="d-flex flex-wrap justify-content-between px-5 py-3">
-  <div class="col-12 col-lg-6 p-1">
+  <div class="col-12 col-lg-6 p-1 brand-container">
 		<a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
 	</div>
 
@@ -10,7 +10,7 @@
 		  'theme_location' => 'primary',
 		  'walker' => new Microdot_Walker_Nav_Menu(),
 		  'container' => false,
-		  'items_wrap' => '<div class="col-12 col-lg-6 text-right p-0">%3$s</div>'
+		  'items_wrap' => '<div class="col-12 col-lg-6 text-right p-0 menu-area">%3$s</div>'
 		));
 		?>
 
