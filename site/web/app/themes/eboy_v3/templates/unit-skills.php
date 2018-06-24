c41735bc4307fb38b02a6b129ddd0beda46eedc7
eboy <giannisduke@gmail.com>
2018-06-24 20:14:28 +0300

    eboy_v3 - skills text

--- site/web/app/themes/eboy_v3/templates/unit-skills.php
+++ site/web/app/themes/eboy_v3/templates/unit-skills.php
@@ -1,7 +1,7 @@
 <section id="skills" class="skills pt-5">
 <div class="container-fluid p-5">
   <div class="d-flex flex-row flex-wrap align-items-start h-100">
-        <div class="col-12 col-md-4 pr-5 pl-0 pt-0 pb-5"><h1>Skills<span class="dot p-1">.</span></h1><span>I am inspired by creating great work with people who are as passionate as I am about building something awesome.</span></div>
+        <div class="col-12 col-md-4 pr-5 pl-0 pt-0 pb-5"><h1>Skills<span class="dot p-1">.</span></h1><span>I enjoy building brands and make them live on paper or on the web, but i can go further than that and deploy super-fast and secure applications on cloud servers.</span></div>
         <div class="col-12 col-md-8 p-0 test">
           <?php if( have_rows('skills') ): ?>
               <div class="d-flex flex-row flex-wrap align-items-center h-100 test">
