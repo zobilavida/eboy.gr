99528e4c72a82dc6eca4ba4e302ace5bde5456b3
eboy <giannisduke@gmail.com>
2018-06-24 20:32:00 +0300

    eboy_v3 - experience text

--- site/web/app/themes/eboy_v3/templates/unit-experience.php
+++ site/web/app/themes/eboy_v3/templates/unit-experience.php
@@ -1,7 +1,7 @@
 <section id="experience" class="experience pt-5">
 <div class="container-fluid p-5">
   <div class="d-flex flex-row flex-wrap align-items-start h-100 test">
-        <div class="col-12 col-md-4 pr-5 pl-0 py-0 test"><h1>Experience<span class="dot p-1">.</span></h1><span>I am inspired by creating great work with people who are as passionate as I am about building something awesome.</span></div>
+        <div class="col-12 col-md-4 pr-5 pl-0 py-0 test"><h1>Experience<span class="dot p-1">.</span></h1><span>I' ve worked with both high-profile and medium to small clients, through all the aspects of design and publishing processes.</span></div>
         <div class="col-12 col-md-8 p-0 test">
           <?php if( have_rows('past_jobs') ): ?>
               <div class="d-flex flex-row flex-wrap align-items-center h-100 test">
