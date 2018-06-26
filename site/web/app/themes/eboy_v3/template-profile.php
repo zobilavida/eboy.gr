1104cc547538fb570a4519e6b5665b7df2876e3c
eboy <giannisduke@gmail.com>
2018-06-26 19:22:32 +0300

    eboy_v3 - profile container

--- site/web/app/themes/eboy_v3/template-profile.php
+++ site/web/app/themes/eboy_v3/template-profile.php
@@ -3,7 +3,9 @@
  * Template Name: Profile Template
  */
 ?>
+<div class="container">
 <?php get_template_part('templates/unit', 'profile'); ?>
 <?php get_template_part('templates/unit', 'skills'); ?>
 <?php get_template_part('templates/unit', 'experience'); ?>
 <?php get_template_part('templates/unit', 'education'); ?>
+</div>
