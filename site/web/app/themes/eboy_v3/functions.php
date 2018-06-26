1676b2e12f56e7018b35b34261e17e726d73b05a
eboy <giannisduke@gmail.com>
2018-06-26 12:21:53 +0300

    eboy_v3 - related products number

--- site/web/app/themes/eboy_v3/functions.php
+++ site/web/app/themes/eboy_v3/functions.php
@@ -413,3 +413,19 @@ if ( $tags ) :
 </div>
 <?php }
 add_action ('eboy_woocommerce_portfolio', 'eboy_woocommerce_categories', 10);
+
+/**
+ * Change number of related products output
+ */
+function woo_related_products_limit() {
+  global $product;
+
+	$args['posts_per_page'] = 6;
+	return $args;
+}
+add_filter( 'woocommerce_output_related_products_args', 'jk_related_products_args' );
+  function jk_related_products_args( $args ) {
+	$args['posts_per_page'] = 6; // 4 related products
+	$args['columns'] = 2; // arranged in 2 columns
+	return $args;
+}
