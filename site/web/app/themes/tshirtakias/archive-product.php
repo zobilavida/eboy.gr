b2e7a6ceaebbb712a510404cf30bef0ed6907f80
eboy <giannisduke@gmail.com>
2018-07-17 01:19:43 +0300

    tshirtakias - change id (8)

--- site/web/app/themes/tshirtakias/archive-product.php
+++ site/web/app/themes/tshirtakias/archive-product.php
@@ -1,7 +1,7 @@
 <div class = "container-fluid">
    <div class = "row row-height">
      <div class = "col-lg-6 display-map left p-0">
-       <a class="btn btn-primary project-preview" href="#" data-project-id="10" role="button">Link</a>
+       <a class="btn btn-primary project-preview" href="#" data-project-id="8" role="button">Link</a>
        <?php //do_action( 'custom_store_finder', 'store_finder' ); ?>
      </div>
      <div class = "col-lg-6 display-results right">
