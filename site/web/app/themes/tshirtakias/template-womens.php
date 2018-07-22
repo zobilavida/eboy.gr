b74690a3412ebd66df1d9a944a1bc79eb4d57b4f
eboy <giannisduke@gmail.com>
2018-07-22 21:40:18 +0300

    tshirtakias - load categories

---
+++ site/web/app/themes/tshirtakias/template-womens.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Template Name: womens Template
+ */
+?>
+
+<div class="d-flex flex-wrap related-products-preview">
+    <?php
+        $args = array( 'post_type' => 'product', 'posts_per_page' => -1, 'product_cat' => 'womens', 'orderby' => 'name', 'order' => 'ASC' );
+
+        $loop = new WP_Query( $args );
+
+        while ( $loop->have_posts() ) : $loop->the_post(); global $product; $id = $product->get_id(); ?>
+
+
+
+          <div class="p-2 product-preview" data-project-id="<?php echo $id; ?>">
+            <?php //the_title(); ?>
+            <?php if (has_post_thumbnail( $loop->post->ID )) echo get_the_post_thumbnail($loop->post->ID, 'shop_catalog'); else echo '<img src="'.woocommerce_placeholder_img_src().'" alt="Placeholder" width="300px" height="300px" />'; ?>
+
+          </div>
+
+
+
+    <?php endwhile; ?>
+    <?php wp_reset_query(); ?>
+</div><!--/.products-->
