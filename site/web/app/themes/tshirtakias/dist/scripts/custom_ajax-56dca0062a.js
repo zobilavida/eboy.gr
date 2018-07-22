14f75079370e78401457d250c9807bc9ee1557f9
eboy <giannisduke@gmail.com>
2018-07-22 17:55:33 +0300

    tshirtakias - load script and from ajax

--- site/web/app/themes/tshirtakias/dist/scripts/custom_ajax-56dca0062a.js
+++
@@ -1,1 +0,0 @@
-jQuery(document).ready(function(t){"use strict";t(".project-preview").on("click",function(){var c=t(this).attr("data-project-id"),e=t(".right");t.ajax({type:"POST",url:singleprojectajax.ajaxurl,data:{action:"load_single_product_content",post_id:c},success:function(t){e.html(t)},complete:function(){},error:function(){}})})});
