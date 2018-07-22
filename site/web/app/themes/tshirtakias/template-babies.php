9cbcac82f4f29014acd81bd5b3e3bfef3f5035b1
eboy <giannisduke@gmail.com>
2018-07-22 21:58:14 +0300

    tshirtakias - related links outside div

--- site/web/app/themes/tshirtakias/template-babies.php
+++ site/web/app/themes/tshirtakias/template-babies.php
@@ -13,12 +13,12 @@
         while ( $loop->have_posts() ) : $loop->the_post(); global $product; $id = $product->get_id(); ?>
 
 
-        <a href="#" class="product-preview" data-project-id="<?php echo $id; ?>">
-          <div class="p-2">
-            <?php //the_title(); ?>
-            <?php if (has_post_thumbnail( $loop->post->ID )) echo get_the_post_thumbnail($loop->post->ID, 'shop_catalog'); else echo '<img src="'.woocommerce_placeholder_img_src().'" alt="Placeholder" width="300px" height="300px" />'; ?>
-          </div>
-        </a>
+        <div class="p-2">
+          <a href="#" class="product-preview" data-project-id="<?php echo $id; ?>">
+          <?php //the_title(); ?>
+          <?php if (has_post_thumbnail( $loop->post->ID )) echo get_the_post_thumbnail($loop->post->ID, 'shop_catalog'); else echo '<img src="'.woocommerce_placeholder_img_src().'" alt="Placeholder" width="300px" height="300px" />'; ?>
+          </a>
+        </div>
 
 
 
--- site/web/app/themes/tshirtakias/template-mens.php
+++ site/web/app/themes/tshirtakias/template-mens.php
@@ -14,13 +14,12 @@
 
 
 
-        <a href="#" class="product-preview" data-project-id="<?php echo $id; ?>">
-          <div class="p-2">
-            <?php //the_title(); ?>
-            <?php if (has_post_thumbnail( $loop->post->ID )) echo get_the_post_thumbnail($loop->post->ID, 'shop_catalog'); else echo '<img src="'.woocommerce_placeholder_img_src().'" alt="Placeholder" width="300px" height="300px" />'; ?>
-          </div>
-        </a>
-
+        <div class="p-2">
+          <a href="#" class="product-preview" data-project-id="<?php echo $id; ?>">
+          <?php //the_title(); ?>
+          <?php if (has_post_thumbnail( $loop->post->ID )) echo get_the_post_thumbnail($loop->post->ID, 'shop_catalog'); else echo '<img src="'.woocommerce_placeholder_img_src().'" alt="Placeholder" width="300px" height="300px" />'; ?>
+          </a>
+        </div>
 
 
     <?php endwhile; ?>
