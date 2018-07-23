a5789335e4f502579466ae89e2967601eefea125
eboy <giannisduke@gmail.com>
2018-07-23 11:06:53 +0300

    tshirtakias - revert override

--- site/web/app/themes/tshirtakias/functions.php
+++ site/web/app/themes/tshirtakias/functions.php
@@ -532,34 +532,3 @@ function kia_email_order_meta_fields( $fields ) {
     return $fields;
 }
 add_filter('woocommerce_email_order_meta_fields', 'kia_email_order_meta_fields');
-
-/**
- * Get HTML for a gallery image.
- *
- * woocommerce_gallery_thumbnail_size, woocommerce_gallery_image_size and woocommerce_gallery_full_size accept name based image sizes, or an array of width/height values.
- *
- * @since 3.3.2
- * @param int $attachment_id
- * @param bool $main_image Is this the main image or a thumbnail?
- * @return string
- */
-function wc_get_gallery_image_html( $attachment_id, $main_image = false ) {
-	$flexslider        = (bool) apply_filters( 'woocommerce_single_product_flexslider_enabled', get_theme_support( 'wc-product-gallery-slider' ) );
-	$gallery_thumbnail = wc_get_image_size( 'gallery_thumbnail' );
-	$thumbnail_size    = apply_filters( 'woocommerce_gallery_thumbnail_size', array( $gallery_thumbnail['width'], $gallery_thumbnail['height'] ) );
-	$image_size        = apply_filters( 'woocommerce_gallery_image_size', $flexslider || $main_image ? 'woocommerce_single': $thumbnail_size );
-	$full_size         = apply_filters( 'woocommerce_gallery_full_size', apply_filters( 'woocommerce_product_thumbnails_large_size', 'full' ) );
-	$thumbnail_src     = wp_get_attachment_image_src( $attachment_id, $thumbnail_size );
-	$full_src          = wp_get_attachment_image_src( $attachment_id, $full_size );
-	$image             = wp_get_attachment_image( $attachment_id, $image_size, false, array(
-		'title'                   => get_post_field( 'post_title', $attachment_id ),
-		'data-caption'            => get_post_field( 'post_excerpt', $attachment_id ),
-		'data-src'                => $full_src[0],
-		'data-large_image'        => $full_src[0],
-		'data-large_image_width'  => $full_src[1],
-		'data-large_image_height' => $full_src[2],
-		'class'                   => $main_image ? 'wp-post-image' : '',
-	) );
-
-	return '<div data-thumb="' . esc_url( $thumbnail_src[0] ) . '" class="woocommerce-product-gallery__image test"><a href="' . esc_url( $full_src[0] ) . '">' . $image . '</a></div>';
-}
