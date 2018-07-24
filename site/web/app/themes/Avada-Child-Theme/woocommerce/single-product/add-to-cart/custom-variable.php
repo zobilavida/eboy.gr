c1c0915e61cb62d175c065d6480d7c6081ea9397
eboy <giannisduke@gmail.com>
2018-07-25 00:16:07 +0300

    tshirtakias

--- site/web/app/themes/Avada-Child-Theme/woocommerce/single-product/add-to-cart/custom-variable.php
+++ site/web/app/themes/Avada-Child-Theme/woocommerce/single-product/add-to-cart/custom-variable.php
@@ -31,7 +31,7 @@ do_action( 'woocommerce_before_add_to_cart_form' ); ?>
 	<?php do_action( 'woocommerce_before_variations_form' ); ?>
 
 	<?php if ( empty( $available_variations ) && false !== $available_variations ) : ?>
-		<p class="stock out-of-stock"><?php esc_html_e( 'This product is currently out of stock and unavailable.', 'woocommerce' ); ?></p>
+
 	<?php else : ?>
 		<table class="variations" cellspacing="0">
 			<tbody>
