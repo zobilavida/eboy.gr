df4fd3eb7d9c65c54d4fc223b5a10e0e39da5cff
eboy <giannisduke@gmail.com>
2018-07-24 11:39:39 +0300

    tshirtakias - product image center

--- site/web/app/themes/Avada-Child-Theme/woocommerce/single-product/product-custom-image.php
+++ site/web/app/themes/Avada-Child-Theme/woocommerce/single-product/product-custom-image.php
@@ -28,6 +28,7 @@ global $product;
 $columns = apply_filters( 'woocommerce_product_thumbnails_columns', 4 );
 $post_thumbnail_id = $product->get_image_id();
 $wrapper_classes   = apply_filters( 'woocommerce_single_product_image_gallery_classes', array(
+	'text-center',
 	'woocommerce-product-gallery',
 	'woocommerce-product-gallery--' . ( has_post_thumbnail() ? 'with-images' : 'without-images' ),
 	'woocommerce-product-gallery--columns-' . absint( $columns ),
