104c4e7bc82b4be1d43ac6d40ffc2da3821fdb65
eboy <giannisduke@gmail.com>
2018-07-25 14:51:16 +0300

    tshirtakias - remove action if is store app

--- site/web/app/themes/Avada-Child-Theme/functions.php
+++ site/web/app/themes/Avada-Child-Theme/functions.php
@@ -186,7 +186,7 @@ add_action( 'woocommerce_before_single_product_custom_summary', 'woocommerce_sho
 //add_action( 'woocommerce_single_custom_product_summary', 'woocommerce_template_single_custom_title', 5 );
 add_action( 'woocommerce_single_custom_product_summary', 'woocommerce_custom_variable_add_to_cart', 30 );
 
-if ( is_page_template('store-app.php') ) {
+if ( is_page('store-app') ) {
   remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title', 5 );
 }
 
