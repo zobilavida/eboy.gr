0bc09538b91febcb18dce45c7c25ffc6dd633165
eboy <giannisduke@gmail.com>
2018-07-25 20:52:10 +0300

    tshi

--- site/web/app/themes/Avada-Child-Theme/js/keepstamp_ajax.js
+++ site/web/app/themes/Avada-Child-Theme/js/keepstamp_ajax.js
@@ -1,7 +1,6 @@
 jQuery(document).ready(function($) {
-    $(document).on('facetwp-loaded', function() {
+  
          var selectedurl = $( "input[name='stamp_url']" ).attr('value');
 
          $('.selected-stamp').attr('src', selectedurl);
- });
  });
