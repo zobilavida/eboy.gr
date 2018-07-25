1f10015e81624de0df3335181e152512bf0ebf36
eboy <giannisduke@gmail.com>
2018-07-25 20:23:07 +0300

    tshirtakias - main js add facet ready

--- site/web/app/themes/Avada-Child-Theme/js/main.js
+++ site/web/app/themes/Avada-Child-Theme/js/main.js
@@ -1,4 +1,6 @@
 jQuery(document).ready(function($) {
+  $(document).on('facetwp-loaded', function() {
+      if (FWP.loaded) {
   $('.product-preview.mens').trigger('click');
   var demourl = 'https://eboy.gr/app/themes/Avada-Child-Theme/img/stamp_placeholder.png';
   $('.selected-stamp').attr('src', demourl);
@@ -36,4 +38,7 @@ jQuery(document).ready(function($) {
     $( "input[name='_custom_option']" ).val( text );
     $('.selected-stamp').attr('src', url);
   });
+
+}
+});
   });
