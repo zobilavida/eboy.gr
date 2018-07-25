929045d059352e91b9f0bc0c7454a344130132b3
eboy <giannisduke@gmail.com>
2018-07-25 20:00:12 +0300

    tshirtakias

--- site/web/app/themes/Avada-Child-Theme/js/custom_ajax.js
+++ site/web/app/themes/Avada-Child-Theme/js/custom_ajax.js
@@ -1,43 +1,44 @@
-jQuery(document).on('facetwp-loaded', function() {
-
+jQuery(document).ready(function($) {
+$(document).on('facetwp-loaded', function() {
 "use strict";
 
-   $('.related-product-preview').on('click',function(){
-       var therelatedId = $(this).attr('data-project-id');
-       var relateddiv = $('.right-product');
+       $('.related-product-preview').on('click',function(){
+           var therelatedId = $(this).attr('data-project-id');
+           var relateddiv = $('.right-product');
 
-       $.ajax({
-           type: "POST",
-           url: singleprojectajax.ajaxurl,
-           data : {action : 'load_single_product_content', post_id: therelatedId },
-           success: function(data){
-               relateddiv.html(data);
-           },
-           complete: function(){
-               loadKeepStampScript();
-               loadsingleproductScript();
-               loadVariationScript();
-               loadFlexSliderScript();
-           },
-           error : function() {
-           }
+           $.ajax({
+               type: "POST",
+               url: singleprojectajax.ajaxurl,
+               data : {action : 'load_single_product_content', post_id: therelatedId },
+               success: function(data){
+                   relateddiv.html(data);
+               },
+               complete: function(){
+                   loadKeepStampScript();
+                   loadsingleproductScript();
+                   loadVariationScript();
+                   loadFlexSliderScript();
+               },
+               error : function() {
+               }
+           });
        });
-   });
 
 
 
 
 function loadsingleproductScript() {
-$.getScript("https://eboy.gr/app/plugins/woocommerce/assets/js/frontend/single-product.min.js?ver=3.3.5");
+   $.getScript("https://eboy.gr/app/plugins/woocommerce/assets/js/frontend/single-product.min.js?ver=3.3.5");
 }
 function loadVariationScript() {
-$.getScript("https://eboy.gr/app/plugins/woocommerce/assets/js/frontend/add-to-cart-variation.min.js?ver=3.3.5");
+   $.getScript("https://eboy.gr/app/plugins/woocommerce/assets/js/frontend/add-to-cart-variation.min.js?ver=3.3.5");
 }
 function loadFlexSliderScript() {
-$.getScript("https://eboy.gr/app/plugins/woocommerce/assets/js/flexslider/jquery.flexslider.min.js?ver=2.6.1");
+   $.getScript("https://eboy.gr/app/plugins/woocommerce/assets/js/flexslider/jquery.flexslider.min.js?ver=2.6.1");
 }
 function loadKeepStampScript() {
-$.getScript("https://eboy.gr/app/themes/tshirtakias/dist/scripts/keepstamp_ajax-7cea83afa4.js?ver=1");
+   $.getScript("https://eboy.gr/app/themes/tshirtakias/dist/scripts/keepstamp_ajax-7cea83afa4.js?ver=1");
 }
 
+});
 });
