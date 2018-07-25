fce9661198c86913b6e5f056f481eab92e8fd2a8
eboy <giannisduke@gmail.com>
2018-07-25 12:23:23 +0300

    tshirtakias

--- site/web/app/themes/Avada-Child-Theme/woocommerce/single-product/product-custom-image.php
+++ site/web/app/themes/Avada-Child-Theme/woocommerce/single-product/product-custom-image.php
@@ -19,7 +19,7 @@
 defined( 'ABSPATH' ) || exit;
 
 // Note: `wc_get_gallery_image_html` was added in WC 3.3.2 and did not exist prior. This check protects against theme overrides being used on older versions of WC.
-if ( ! function_exists( 'wc_custom_get_gallery_image_html' ) ) {
+if ( ! function_exists( 'wc_get_gallery_image_html' ) ) {
 	return;
 }
 
