a3bbc7ff1e2d3fd289f6ef654b969926a70eb4c0
eboy <giannisduke@gmail.com>
2018-07-25 20:49:32 +0300

    tshirtakias - keepstamp facet loaded

--- site/web/app/themes/Avada-Child-Theme/js/keepstamp_ajax.js
+++ site/web/app/themes/Avada-Child-Theme/js/keepstamp_ajax.js
@@ -1,5 +1,7 @@
 jQuery(document).ready(function($) {
+    $(document).on('facetwp-loaded', function() {
          var selectedurl = $( "input[name='stamp_url']" ).attr('value');
 
          $('.selected-stamp').attr('src', selectedurl);
+ });
  });
