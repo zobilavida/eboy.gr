5f6093ab99ce001d491d2480520f391a8058c571
eboy <giannisduke@gmail.com>
2018-07-26 19:12:52 +0300

    tahirtakias wc_custom_get_gallery_image_html

--- site/web/app/themes/Avada-Child-Theme/woocommerce/single-product/product-custom-image.php
+++ site/web/app/themes/Avada-Child-Theme/woocommerce/single-product/product-custom-image.php
@@ -40,7 +40,7 @@ $wrapper_classes   = apply_filters( 'woocommerce_single_product_image_gallery_cl
 	<figure class="woocommerce-product-gallery__wrapper">
 		<?php
 		if ( has_post_thumbnail() ) {
-			$html  = wc_get_gallery_image_html( $post_thumbnail_id, true );
+			$html  = wc_custom_get_gallery_image_html( $post_thumbnail_id, true );
 		} else {
 			$html  = '<div class="woocommerce-product-gallery__image--placeholder">';
 			$html .= sprintf( '<img src="%s" alt="%s" class="wp-post-image" />', esc_url( wc_placeholder_img_src() ), esc_html__( 'Awaiting product image', 'woocommerce' ) );
