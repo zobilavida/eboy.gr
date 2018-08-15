793aa173ac36ce209765b40a7e1f2f8d1bf2796d
eboy <giannisduke@gmail.com>
2018-08-15 22:22:58 +0300

    telesphorus_sage - add kirki plugin

---
+++ site/web/app/themes/i-one/inc/kirki/field/class-kirki-field-spacing.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Override field methods
+ *
+ * @package     Kirki
+ * @subpackage  Controls
+ * @copyright   Copyright (c) 2017, Aristeides Stathopoulos
+ * @license     http://opensource.org/licenses/https://opensource.org/licenses/MIT
+ * @since       2.2.7
+ */
+
+/**
+ * Field overrides.
+ */
+class Kirki_Field_Spacing extends Kirki_Field_Dimensions {
+
+	/**
+	 * Set the choices.
+	 * Adds a pseudo-element "controls" that helps with the JS API.
+	 *
+	 * @access protected
+	 */
+	protected function set_choices() {
+
+		$default_args = array(
+			'controls' => array(
+				'top'    => ( isset( $this->default['top'] ) ),
+				'bottom' => ( isset( $this->default['top'] ) ),
+				'left'   => ( isset( $this->default['top'] ) ),
+				'right'  => ( isset( $this->default['top'] ) ),
+			),
+			'labels'   => array(
+				'top'    => esc_attr__( 'Top', 'i-one' ),
+				'bottom' => esc_attr__( 'Bottom', 'i-one' ),
+				'left'   => esc_attr__( 'Left', 'i-one' ),
+				'right'  => esc_attr__( 'Right', 'i-one' ),
+			),
+		);
+
+		$this->choices = wp_parse_args( $this->choices, $default_args );
+
+	}
+}
