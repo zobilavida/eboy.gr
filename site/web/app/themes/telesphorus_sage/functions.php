a3e09451229d4c53ed8bbe77d6e27709f5ff6cc9
eboy <giannisduke@gmail.com>
2018-08-17 22:37:25 +0300

    telesphorus_sage - test custom widget

--- site/web/app/themes/telesphorus_sage/functions.php
+++ site/web/app/themes/telesphorus_sage/functions.php
@@ -89,3 +89,48 @@ function get_hero_content(){
 }
 }
 add_action ('telesphorus_hero', 'get_hero_content');
+
+
+class jpen_Example_Widget extends WP_Widget {
+  /**
+  * To create the example widget all four methods will be
+  * nested inside this single instance of the WP_Widget class.
+  **/
+	public function __construct() {
+	    $widget_options = array(
+	      'classname' => 'example_widget',
+	      'description' => 'This is an Example Widget',
+	    );
+	    parent::__construct( 'example_widget', 'Example Widget', $widget_options );
+	  }
+
+		public function widget( $args, $instance ) {
+  $title = apply_filters( 'widget_title', $instance[ 'title' ] );
+  $blog_title = get_bloginfo( 'name' );
+  $tagline = get_bloginfo( 'description' );
+  echo $args['before_widget'] . $args['before_title'] . $title . $args['after_title']; ?>
+
+  <p><strong>Site Name:</strong> <?php echo $blog_title ?></p>
+  <p><strong>Tagline:</strong> <?php echo $tagline ?></p>
+
+  <?php echo $args['after_widget'];
+}
+
+public function form( $instance ) {
+  $title = ! empty( $instance['title'] ) ? $instance['title'] : ''; ?>
+  <p>
+    <label for="<?php echo $this->get_field_id( 'title' ); ?>">Title:</label>
+    <input type="text" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo esc_attr( $title ); ?>" />
+  </p><?php
+}
+
+public function update( $new_instance, $old_instance ) {
+$instance = $old_instance;
+$instance[ 'title' ] = strip_tags( $new_instance[ 'title' ] );
+return $instance;
+}
+}
+function jpen_register_example_widget() {
+  register_widget( 'jpen_Example_Widget' );
+}
+add_action( 'widgets_init', 'jpen_register_example_widget' );
